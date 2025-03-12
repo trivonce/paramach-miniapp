29357b5d63c81063f286ccd098c1e2bc2f408eb9
UlugbekTemirov <temirovulugbek2003@gmail.com>
2025-03-12 15:42:47 +0500

    changes;

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -12,11 +12,14 @@ const ProductCard = (props: Props) => {
 
   return (
     <div className="rounded-2xl overflow-hidden shadow-md mb-2 dark:bg-gray-900">
+      <div className="relative z-[1] overflow-hidden">
+        <img className="absolute top-0 left-0 w-full h-full -z-[1] blur-sm" src={image} alt="placholder_image" />
       <img
-        className="h-[140px] w-full object-cover"
+        className="h-[140px] w-full object-contain"
         src={image || "https://placehold.co/600x400"}
         alt="paramach"
       />
+      </div>
 
       <div className="px-3 py-2">
         <h1 className="text-tp-main font-medium line-clamp-2 text-sm leading-4 h-8">{name || 'Placeholder'}</h1>
