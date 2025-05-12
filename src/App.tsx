b16e4363e6cfc58eb6b6d83b31aa779fffda1624
UlugbekTemirov <temirovulugbek2003@gmail.com>
2025-05-12 19:09:10 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
 
 import { AnimatePresence } from "framer-motion";
 import { Toaster } from 'sonner';
